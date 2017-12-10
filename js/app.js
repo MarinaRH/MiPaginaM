872ab667a0a76c450561973949de2f62959936f6
MarinaRH <l_marina1717@hotmail.com>
2017-12-10 13:53:25 -0500

    agregando funcion para mostrar y ocultar menu

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var boton = document.getElementById("menu");
+function showMenu(){
+	var menu = document.getElementById("opc-menu");
+	if(menu.classList.contains("disabled-menu")){
+		menu.classList.remove("disabled-menu");
+		menu.classList.add("enabled-menu");
+    }else{
+			menu.classList.remove("enabled-menu");
+			menu.classList.add("disabled-menu");
+    }
+}
+boton.addEventListener("click",showMenu);
